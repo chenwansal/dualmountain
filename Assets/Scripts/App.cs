fe6928729b58b19de7533b72f4efd5267e019c09
杰克有茶 <chenwansal1@163.com>
2022-05-27 13:08:57 +0800

    <asset> 导入: 山

--- Assets/Scripts/App.cs
+++ Assets/Scripts/App.cs
@@ -2,17 +2,38 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class App : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+namespace DualMountain {
+
+    public class App : MonoBehaviour {
+
+        void Awake() {
+            Debug.Log("Hello World");
+        }
+
+        void Start() {
+
+        }
+
+        void Update() {
+
+        }
+
+        void FixedUpdate() {
+
+        }
+
+        void LateUpdate() {
+
+        }
+
+        void OnDestroy() {
+
+        }
+
+        void OnApplicationQuit() {
+
+        }
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
+
